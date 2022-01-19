1ff02fe6633cb3c4f4f8dc6e586ba082e1261842
Александр Иншаков <a.inshakov84@gmail.com>
2022-01-19 17:37:33 +0300

    Добавьте файлы проекта.

---
+++ zd 5 ur 5/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zd_5_ur_5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter= "Тектовые файлы (*.txt)|*.txt| все файлы(*.*) | *.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox1.Text= File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+       saveFileDialog.Filter= "Тектовые файлы (*.txt)|*.txt| все файлы(*.*) | *.*";
+        if(saveFileDialog.ShowDialog()==true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox1.Text);
+            }
+        }
+    }
+}
